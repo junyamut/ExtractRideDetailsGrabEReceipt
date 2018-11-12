f4421cca8856e07277eeff94b76b3bc148324cdf
Jose Yamut <junyamut@users.noreply.github.com>
2018-11-12 18:52:25 +0800

    Added handling of data formant and cell type to data values

--- src/main/java/WriteCell.java
+++ src/main/java/WriteCell.java
@@ -1,5 +1,9 @@
+import java.util.Date;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.CreationHelper;
 import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.IndexedColors;
@@ -9,10 +13,11 @@ import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-public class WriteCell {		
+public class WriteCell {	
 	private static final HorizontalAlignment DEFAULT_HORIZONTAL_ALIGNMENT = HorizontalAlignment.LEFT;
 	private static final IndexedColors DEFAULT_INDEX_COLOR = IndexedColors.BLACK;
 	private static final FillPatternType DEFAULT_FILL_PATTERN_TYPE = FillPatternType.NO_FILL;
+	public static enum DataType {Text, NumericDecimal, DateTime, Empty};
 	protected Sheet sheet;
 	protected Cell cell;	
 
@@ -34,21 +39,52 @@ public class WriteCell {
 	public static final class WriteCellBuilder {
 		private XSSFWorkbook workbook;
 		private Sheet sheet;
-		private String value;
-		private XSSFFont font;
+		private Object value;
+		private XSSFFont font;		
+		private CellType type;
 		private CellStyle style;
 		private Cell cell;
 		private HorizontalAlignment horizontalAlignment;
 		private FillPatternType fillPatternType;
-		private IndexedColors indexedColor;
+		private IndexedColors indexedColor;		
 		private Integer columnIndex, columnWidth;
 		private Integer[] cellRangeIndices;
+		private short dataFormat;
+		private CreationHelper creationHelper;
 		
 		public WriteCellBuilder(XSSFWorkbook workbook, Sheet sheet, Row row, int column) {
+			this.creationHelper = workbook.getCreationHelper();
 			this.workbook = workbook;
 			this.sheet = sheet;
 			this.style = workbook.createCellStyle();
 			this.cell = row.createCell(column);
+			this.dataFormat = creationHelper.createDataFormat().getFormat("General");
+			this.type = CellType.STRING;
+			this.font = new SpreadsheetFont.SpreadsheetFontBuilder(workbook).build().getFont();
+			this.horizontalAlignment = DEFAULT_HORIZONTAL_ALIGNMENT;
+			this.indexedColor = DEFAULT_INDEX_COLOR;
+			this.fillPatternType = DEFAULT_FILL_PATTERN_TYPE;
+		}
+		
+		public WriteCellBuilder cellStyleDataType(DataType dataType) {
+			switch (dataType) {
+				case NumericDecimal:
+					this.type = CellType.NUMERIC;
+					this.dataFormat = creationHelper.createDataFormat().getFormat("#,##0.00");
+					break;
+				case DateTime:
+					this.type = CellType.NUMERIC;
+					this.dataFormat = creationHelper.createDataFormat().getFormat("yyyy-mm-dd HH:mm");
+					break;
+				case Empty:
+					this.type = CellType.BLANK;
+					break;
+				case Text:
+				default:
+					this.type = CellType.STRING;
+					
+			}
+			return this;
 		}
 		
 		public WriteCellBuilder cellColumnWidth(Integer columnIndex, Integer columnWidth) {
@@ -62,8 +98,8 @@ public class WriteCell {
 			return this;
 		}
 		
-		public WriteCellBuilder value(String value) {
-			this.value = value;
+		public WriteCellBuilder value(Object object) {
+			this.value = object;
 			return this;
 		}
 		
@@ -88,30 +124,29 @@ public class WriteCell {
 		}			
 		
 		public WriteCell build() {
-			if (this.font == null) {
-				this.font = new SpreadsheetFont.SpreadsheetFontBuilder(workbook).build().getFont();
-			}
-			if (this.horizontalAlignment == null) {
-				this.horizontalAlignment = DEFAULT_HORIZONTAL_ALIGNMENT;				
-			}
-			if (this.indexedColor == null) {
-				this.indexedColor = DEFAULT_INDEX_COLOR;
-			}
-			if (this.fillPatternType == null) {
-				this.fillPatternType = DEFAULT_FILL_PATTERN_TYPE;
-			} 
 			if (this.columnIndex != null && this.columnWidth != null) {
 				sheet.setColumnWidth(this.columnIndex, this.columnWidth);
 			}
 			if (this.cellRangeIndices != null && this.cellRangeIndices.getClass().isArray() && this.cellRangeIndices.length == 4) {
 				sheet.addMergedRegion(new CellRangeAddress(this.cellRangeIndices[0], this.cellRangeIndices[1], this.cellRangeIndices[2], this.cellRangeIndices[3]));
 			}
+			if (this.value instanceof String) {
+				cell.setCellValue(this.value.toString());
+			} else if (this.value instanceof Double) {
+				Double value = (double) this.value;
+				cell.setCellValue(value.doubleValue()); 
+			} else if (this.value instanceof Date) {
+				Date value = (Date) this.value;
+				cell.setCellValue(value);
+			}
 			style.setFont(this.font);
 			style.setAlignment(this.horizontalAlignment);
 			style.setFillForegroundColor(this.indexedColor.getIndex());
 			style.setFillPattern(this.fillPatternType);
-			cell.setCellValue(this.value);
+			style.setDataFormat(this.dataFormat);
 			cell.setCellStyle(this.style);
+			cell.setCellType(this.type);			
+			
 			return new WriteCell(this);
 		}
 	}
