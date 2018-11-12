e2ea43b5ee9801ac63bccfb682e20dbc1df60e23
Jose Yamut <junyamut@users.noreply.github.com>
2018-11-12 18:54:24 +0800

    Set data type for cell values when building the cell

--- src/main/java/SpreadsheetWriter.java
+++ src/main/java/SpreadsheetWriter.java
@@ -1,11 +1,11 @@
-//import java.text.DateFormat;
-//import java.text.SimpleDateFormat;
-//import java.time.LocalDateTime;
-//import java.time.format.DateTimeFormatter;
-//import java.util.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.Map;
 
+import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
@@ -38,31 +38,37 @@ public class SpreadsheetWriter {
 	    return true;
 	}
 	
-	public void writeData() {		
+	public void writeData() throws ParseException {		
 		if (!isRowEmpty(row)) {
 			throw new IllegalStateException("Row is not empty. Aborting write.");
-		}		
-		Iterator<Map.Entry<String, Object>> iterator = dataMap.entrySet().iterator();
-		int column = 0; 
+		}
+		DateFormat read = new SimpleDateFormat("dd MMM yy HH:mm Z");
+		int column = 0;
+		int numericColumnsStart = SpreadsheetTemplate.SUBLABELS_BD_LIST.size() + 1;
+		Iterator<Map.Entry<String, Object>> iterator = dataMap.entrySet().iterator();		
 		while (iterator.hasNext()) {
 			Map.Entry<String, Object> data = iterator.next();
 			System.out.println(data.getKey() + ": " + data.getValue());
+			// The column is the Header index value from SpreadsheetTemplate List<String> SUBLABELS_BD_LIST & List<String> SUBLABELS_RS_LIST combined
 			column = SpreadsheetTemplate.getColumnHeaderIndex(data.getKey().toString().trim());
+			// If column is found it should have an index >= 0
+			// Pick-up time & TOTAL are inserted on Columns 0 & 1 which is not in the combined list above
+			// Add 2 to the value of column to move it 2 Columns to the right
 			if (column >= 0) {
-				new WriteCell.WriteCellBuilder(workbook, sheet, row, (column + 2)).value(data.getValue().toString()).build();
+				if (column <= numericColumnsStart) {					
+					new WriteCell.WriteCellBuilder(workbook, sheet, row, (column + 2)).value(data.getValue()).build();
+				} else {
+					new WriteCell.WriteCellBuilder(workbook, sheet, row, (column + 2)).value(data.getValue()).cellStyleDataType(WriteCell.DataType.NumericDecimal).build();
+				}
 			}
-			if (data.getKey().equals("Pick-up time")) { // 27 Oct 18 16:13 +0800
-//				DateFormat read = new SimpleDateFormat("dd MMM yy HH:mm Z");
-//				DateFormat write = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-//				Date date = read.parse(data.getValue().toString());				
-//				System.out.println(write.format(date));
-				new WriteCell.WriteCellBuilder(workbook, sheet, row, 0).value(data.getValue().toString()).build();
+			if (data.getKey().equals("Pick-up time")) {				
+				Date date = read.parse(data.getValue().toString());
+				new WriteCell.WriteCellBuilder(workbook, sheet, row, 0).value(date).cellStyleDataType(WriteCell.DataType.DateTime).cellStyleFont(new SpreadsheetFont.SpreadsheetFontBuilder(workbook).color(HSSFColor.HSSFColorPredefined.BLUE.getIndex()).height(12).bold(true).build().getFont()).build();
 			}
 			if (data.getKey().equals("TOTAL")) {
-				new WriteCell.WriteCellBuilder(workbook, sheet, row, 1).value(data.getValue().toString()).build();
-			}
+				new WriteCell.WriteCellBuilder(workbook, sheet, row, 1).value(data.getValue()).cellStyleDataType(WriteCell.DataType.NumericDecimal).cellStyleFont(new SpreadsheetFont.SpreadsheetFontBuilder(workbook).color(HSSFColor.HSSFColorPredefined.RED.getIndex()).height(12).bold(true).build().getFont()).build();
+			}			
 			column++;
 		}
 	}
 }
-
